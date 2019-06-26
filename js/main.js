abb6661ec1b516cf76d106e06887949590841602
yang8629 <8629yang@gmail.com>
2019-06-27 03:16:13 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     $('#btn-read').click(function(event) {
         read();
     });
+    $('#btn-answer').click(function(event) {
+        answer();
+    });
 });
 
 function save() {
@@ -42,8 +45,20 @@ function read() {
     parameter.sheetUrl = "https://docs.google.com/spreadsheets/d/1doUyHaiaXS1ZNRBhSbw3xdH4pVwnV60_N8vy-6kUQpw/edit?usp=sharing";
     parameter.sheetTag = "工作表1";
     $.get(postURL, parameter, function(data) {
-
         alert(data.who + '在' + data.where + data.what);
+    });
+}
+
+function answer() {
+    let parameter = new Object();
+    let postURL = "https://script.google.com/macros/s/AKfycbzK8LwoI81yGYSODMQkTB7ytgqrYDj7a44Y38uB/exec";
+    let count;
+    parameter.method = "read2";
+    parameter.sheetUrl = "https://docs.google.com/spreadsheets/d/1doUyHaiaXS1ZNRBhSbw3xdH4pVwnV60_N8vy-6kUQpw/edit?usp=sharing";
+    parameter.sheetTag = "工作表2";
+    $.get(postURL, parameter, function(data) {
+
+        alert(data.answer);
     });
 }
 
